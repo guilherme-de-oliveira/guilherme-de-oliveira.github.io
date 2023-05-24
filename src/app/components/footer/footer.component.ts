0447e2110c1b5241f9e13aed1617bd2afae5dcc3
Guilherme Henrique de Oliveira <guihenrique_oliveira@hotmail.com>
2023-05-24 17:20:05 -0300

    Last changes
    - Added icons
    - Changed footer style
    - Clean code

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MenuItem } from 'primeng/api';
+import contacts from '../../../assets/contacts.json';
 
 @Component({
   selector: 'app-footer',
@@ -7,14 +7,5 @@ import { MenuItem } from 'primeng/api';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-  github: string = `guilherme-de-oliveira`;
-  linkedin: string =`guilherme-h-de-oliveira`;
-  email: string =`guihenrique_oliveira@hotmail.com`;
-
-  items: MenuItem[] = [
-    {title: 'Github', label: `${this.github}`, icon: 'pi pi-fw pi-github', url: 'https://github.com/guilherme-de-oliveira'},
-    {title: 'Linkedin', label: `${this.linkedin}`, icon: 'pi pi-fw pi-linkedin', url: 'https://www.linkedin.com/in/guilherme-h-de-oliveira'},
-    {title: 'Email', label: `${this.email}`, icon: 'pi pi-fw pi-id-card', url: 'guihenrique_oliveira@hotmail.com'},
-    {title: 'Medium', label: `${this.github}`, icon: 'pi pi-fw pi-pencil', url: 'https://medium.com/@guilherme-de-oliveira'},
-  ];
+  items = contacts.data;
 }
